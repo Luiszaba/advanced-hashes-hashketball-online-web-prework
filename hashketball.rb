676fa0588bf1ed358551dd163b6b59e816a6f5e2
Luis Zabala <luis.zabala076@gmail.com>
2019-01-13 18:45:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,6 +177,7 @@ def player_numbers(teams)
   if game_hash[:home].value? (teams)
     game_hash[:home][:players].map do |names, stats|
       stats.values.first
+    end
       elsif
       game_hash[:away].value? (teams)
       game_hash[:away][:players].map do |names, stats|
